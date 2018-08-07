9cf46662928cdd0ab32340c2136826d25e9fc135
tisufa <tisufamoney@gmail.com>
2018-08-07 10:02:36 +0700

    edit convertToObjectId

--- components/base/convertToObjectId.js
+++ components/base/convertToObjectId.js
@@ -1,24 +1,24 @@
 'use strict';
+const ObjectId = require('mongodb').ObjectID;
+module.exports = function (entity) {
+    /**
+     * Employee objectId
+     */
+    let objEntity = [
+        'created_by',
+        'updated_by',
+        'm_company_id',
+        'parent_id',
+        'm_role_id',
+        'm_menu_id',
+        'm_employee_id'
+    ];
 
-const ObjectId = require('mongodb').ObjectId;
-
-module.exports = function(entity){
-  if (entity.m_menu_id) {
-    entity.m_menu_id = ObjectId(entity.m_menu_id);
-}
-if (entity.m_company_id) {
-  entity.m_company_id = ObjectId(entity.m_company_id);
-}
-if (entity.parent_id) {
-  entity.parent_id = ObjectId(entity.parent_id);
-}
-if (entity.m_role_id) {
-  entity.m_role_id = ObjectId(entity.m_role_id);
-}
-if (entity.m_employee_id) {
-  entity.m_employee_id = ObjectId(entity.m_employee_id);
-}
-if (entity.m_unit_id) {
-  entity.m_unit_id = ObjectId(entity.m_unit_id);
-}
+    for (let key in entity) {
+        let check = objEntity.includes(key);
+        if (check) {
+            let value = entity[key];
+            entity[key] = ObjectId(value);
+        }
+    }
 }
