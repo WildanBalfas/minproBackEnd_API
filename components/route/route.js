e0c15df2b2b7158079603a76a3fe898196db6b62
Rozinul Mahrus <rozinulmahrus@gmail.com>
2018-08-07 17:38:14 +0700

    add m_role

--- components/route/route.js
+++ components/route/route.js
@@ -2,6 +2,7 @@
 
 module.exports = exports = function (server) {
     require('../controllers/template.controller')(server, 'm_menu');
+    require('../controllers/template.controller')(server, 'm_role');
     require('../controllers/template.controller')(server, 'm_unit');
     require('../controllers/template.controller')(server, 'm_menu_access');
     require('../controllers/template.controller')(server, 'm_company');
