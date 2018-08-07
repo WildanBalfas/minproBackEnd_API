de942173590d2799e6f04235297501e0c67c0f24
dimaszsiddik <zaparsiddik@gmail.com>
2018-08-07 14:18:09 +0700

    tambah route

--- components/route/route.js
+++ components/route/route.js
@@ -6,4 +6,7 @@ module.exports = exports = function (server) {
     require('../controllers/template.controller')(server, 'm_menu_access');
     require('../controllers/template.controller')(server, 'm_company');
     require('../controllers/template.controller')(server, 'm_souvenir');
+    require('../controllers/template.controller')(server, 'm_product');
+    require('../controllers/template.controller')(server, 't_souvenir');
+
 }
