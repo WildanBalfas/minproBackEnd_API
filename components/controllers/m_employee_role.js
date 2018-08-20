e9d32d33bd2c6dd5b2a5206c62cb25f0e5e58c4b
srireskianita <srireskianita@gmail.com>
2018-08-20 15:05:50 +0700

    tambah aggregation employee

--- components/controllers/m_employee_role.js
+++ components/controllers/m_employee_role.js
@@ -40,4 +40,36 @@ module.exports=exports= function(server){
            });
        });
    });
+
+   server.get('/api/m_employee_role_requester', (req, res, next) => {
+    MongoClient.connect(config.dbconn, function (err, db){
+        if (err) throw err;
+        dbo = db.db(config.dbname);
+        dbo.collection(name)
+        .aggregate([
+         { $lookup: { from: "m_user", localField: "_id", foreignField: "m_employee_id", as: "userDoc" } },
+         { $lookup: { from: "m_role", localField: "userDoc.m_role_id", foreignField: "_id", as: "roleDoc" } },
+         { $unwind: "$userDoc" },
+         { $unwind: "$roleDoc" },
+        //  { $match: {"roleDoc.name": /Requester/ }},
+         {
+             $project: {
+                 "_id": 1,
+                 "code": "$code",
+                 "name":{
+                     "first":"$first_name",
+                     "last" :"$last_name"
+                 },
+                 "roleName":"$roleDoc.name",
+                 "roleId": "$roleDoc._id"
+             }
+         }
+     ])
+        .toArray(function (err, response) {
+            if (err) throw err;
+            res.send(200, response);
+            db.close();
+        });
+    });
+});
 }
